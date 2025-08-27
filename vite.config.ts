4efdb7f36dd5141ff6817dfc47486f38f49ccbe9
Edison Vargas Teixeira <edison.teixeira@ska.com.br>
2025-08-27 13:19:43 -0300

    commitando ajuste no base do arquivo vte.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/encodlab-studio-v2.0/' : '/',
+  base: mode === 'production' ? './encodlab-studio-v2.0/' : '/',
   server: {
     host: "::",
     port: 8080,
